6a910b29cd282fd2cb44d14b081449bb279d9b46
Alexander <androidldz@gmail.com>
2015-12-02 22:23:40 +0100

    fixed payload include

--- include/payload460deh/payload_460deh.h
+++ include/payload460deh/payload_460deh.h
@@ -10,10 +10,10 @@
 #define SYS36_PAYLOAD -1
 #define WANIN_PAYLOAD -2
 
-extern int is_firm_460DEH(void);
-extern void set_bdvdemu_460DEH(int current_payload);
-extern void load_payload_460DEH(int mode);
-extern int is_payload_loaded_460DEH(void);
+extern int is_firm_460deh(void);
+extern void set_bdvdemu_460deh(int current_payload);
+extern void load_payload_460deh(int mode);
+extern int is_payload_loaded_460deh(void);
 
 //#define CONFIG_USE_SYS8PERMH4 //disabled by default (testing, maybe not added on final release - if not usefull)
 //#define CONFIG_USE_SYS8CONFIG //disabled, not working yet
